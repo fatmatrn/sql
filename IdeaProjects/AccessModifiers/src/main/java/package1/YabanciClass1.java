502752677fe767087c65a0ea2adf1822f7333727
Fatmatrn <ftmtrn3488@gmail.com>
2022-11-27 10:54:35 +0300

    V1

--- IdeaProjects/AccessModifiers/src/main/java/package1/YabanciClass1.java
+++ IdeaProjects/AccessModifiers/src/main/java/package1/YabanciClass1.java
@@ -2,7 +2,7 @@ package package1;
 
 public class YabanciClass1 {
     public static void main(String[] args) {
-        ParentClass parentClass = new ParentClass();
+
 
 
     }
--- IdeaProjects/AccessModifiers/src/main/java/package2/YabanciClass2.java
+++ IdeaProjects/AccessModifiers/src/main/java/package2/YabanciClass2.java
@@ -4,7 +4,7 @@ import package1.ParentClass;
 
 public class YabanciClass2 {
     public static void main(String[] args) {
-ParentClass parentClass = new ParentClass();
+
 
     }
 }
