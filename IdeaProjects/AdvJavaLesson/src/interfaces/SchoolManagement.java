9bb9067a25a6efc52adaf3fc6492965526cc15a6
Fatmatrn <ftmtrn3488@gmail.com>
2022-12-15 22:27:02 +0300

    15.12

---
+++ IdeaProjects/AdvJavaLesson/src/interfaces/SchoolManagement.java
@@ -0,0 +1,19 @@
+package interfaces;
+
+
+/*Task: Okul yönetimi için öğrenci ve öğretmen kayıtlarını
+       -oluşturan
+       -görüntüleyen
+       -güncelleyen
+       -silen
+       bir program yazınız.
+     */
+public class SchoolManagement {
+    public static void main(String[] args) {
+        Student student = new Student("Tolstoy");
+        Teacher teacher = new Teacher("Dostoyevski");
+
+
+    }
+}
+
